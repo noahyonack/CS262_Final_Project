bbf71d2485dd7dac764e520f3feea7bbc0590977
Noah <noah.yonack@gmail.com>
2016-04-15 17:54:12 -0400

    Changed directory structure

--- Parallelogram/build/lib/parallelogram/__init__.py
+++
@@ -1,5 +0,0 @@
-''' 
-This file tells Python to treat the directory (Parallelogram, in this case)
-as a package. The only code (if any) that this file should contain should
-be related initialization.
-'''
