7d9aa618ad9759ff6c5ea00fd66648df8e03d789
Noah <noah.yonack@gmail.com>
2016-04-24 23:13:21 -0400

    Fixed indexing bug in _single_filter()

--- tests/local/testFilter.py
+++ tests/local/testFilter.py
@@ -7,10 +7,23 @@ from parallelogram import helpers
 
 class TestFilter(unittest.TestCase):
 
-	def test_filter(self):
+	def test_filter_1(self):
 		
 		def test1(elt, index):
 			return elt % 2 == 0
+
+		output_1 = helpers._single_filter(test1, [1,2,3,4,5,6])
+		self.assertEqual(output_1, [2,4,6])
+
+		output_2 = helpers._single_filter(test1, [])
+		self.assertEqual(output_2, [])
 		
-		output = helpers._single_filter(test1, [1,2,3,4,5,6])
-		self.assertEqual(output, [2,4,6])
+
+	def test_filter_2(self):
+
+		def test2(elt, index):
+			return elt == -1 
+
+		big_list = range(10000)
+		output_3 = helpers._single_filter(test2, big_list)
+		self.assertEqual(output_3, [])
