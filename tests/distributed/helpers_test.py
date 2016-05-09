c247ff4f72be12a9793598ebe8935dfa72da707a
Noah <noah.yonack@gmail.com>
2016-05-08 23:37:42 -0400

    Deleted old test file

--- tests/distributed/helpers_test.py
+++
@@ -1,7 +0,0 @@
-#class to test if cloudpickle can handle externally defined classes
-class testClass():
-    def __init__(self, value):
-        self.value = value
-
-    def add(self):
-        return(self.value + 1)
